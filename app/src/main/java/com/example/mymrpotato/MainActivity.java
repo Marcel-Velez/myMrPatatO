a9365ce67bf4580ae49bed4124dbd4570d021c2b
Marcel-Velez <marcel.velez1997@hotmail.com>
2019-02-08 10:45:51 +0100

    bezig met memmory

--- app/src/main/java/com/example/mymrpotato/MainActivity.java
+++ app/src/main/java/com/example/mymrpotato/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.mymrpotato;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkClicked(View v) {
-        Log.d( tag: "potato", smg: "checkClicked");
+        CheckBox checkbox = (CheckBox) v;
+        // get text of checkbox
+        String checkText = "potato" + checkbox.getText().toString();
+
+        // get if checked
+        Boolean checked = checkbox.isChecked();
+        int resID = getResources().getIdentifier(checkText, "id", getPackageName());
+
+        if (checked) {
+            String res = (String) resID;
+            Log.d("potato",(String) checkText);
+            ImageView image = (ImageView) findViewById(resID);
+            image.setVisibility(View.VISIBLE);
+        } else {
+            Log.d("potato","unchecked");
+            ImageView image = (ImageView) findViewById(resID);
+            image.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void onSaveInstanceState(currentState) {
+        super.onSaveInstanceState(savedInstanceState);
+        (GridLayout) findViewById(R.id.RelativeLayout01);
+
+        int count = mlayout.getChildCount();
+        for(int i = 0 ; i <count ; i++){
+            View checkbox = mlayout.getChildAt(i);
+            String checkText = "potato" + checkbox.getText().toString();
+            Boolean checked = checkbox.isChecked();
+
+            savedInstanceState.putBoolean(checkText, checked);
+        }
+
     }
 }
